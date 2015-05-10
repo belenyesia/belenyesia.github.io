0f3b1d44e9bbc34566003a095716e3e41b81237c
ev <ericvautier@users.noreply.github.com>
2015-05-10 21:13:14 +0100

    Content changes

---
+++ js/send.js
@@ -0,0 +1,42 @@
+jQuery(function($)  
+{
+    $("#contact_form").submit(function()
+    {
+        var email = $("#email").val(); // get email field value
+        var name = $("#name").val(); // get name field value
+        var msg = $("#msg").val(); // get message field value
+        $.ajax(
+        {
+            type: "POST",
+            url: "https://mandrillapp.com/api/1.0/messages/send.json",
+            data: {
+                'key': 'fHtk1BA8VN7J8Nu-NlZYlA',
+                'message': {
+                    'from_email': email,
+                    'from_name': name,
+                    'headers': {
+                        'Reply-To': email
+                    },
+                    'subject': 'Website Contact Form Submission',
+                    'text': msg,
+                    'to': [
+                    {
+                        'email': 'eric.vautier@gmail.com',
+                        'name': 'Eric',
+                        'type': 'to'
+                    }]
+                }
+            }
+        })
+        .done(function(response) {
+            alert('Your message has been sent. Thank you!'); // show success message
+            $("#name").val(''); // reset field after successful submission
+            $("#email").val(''); // reset field after successful submission
+            $("#msg").val(''); // reset field after successful submission
+        })
+        .fail(function(response) {
+            alert('Error sending message.');
+        });
+        return false; // prevent page refresh
+    });
+});
