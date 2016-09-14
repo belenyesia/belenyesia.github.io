6914c43d43918d600ccc337c56f5116ebe72b9ef
ev <eric.vautier+anm@gmail.com>
2016-09-14 01:06:09 +0100

    Integrated Google Form and Google Analytics tracking

--- js/send.js
+++
@@ -1,6 +0,0 @@
-var submitted=false;
-
-$('#mG61Hd').on('submit', function(e) {
-  $('#mG61Hd *').fadeOut(2000);
-  $('#mG61Hd').prepend('Thank you for your request. I will be in touch as soon as I can!');
-});
